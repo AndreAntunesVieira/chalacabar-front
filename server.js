411838b0a60bb8723877b97f2ef93a2e4e3805a9
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-24 14:18:34 -0300

    Remove force ssl

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const next = require('next')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const forceSSL = require('express-force-ssl')
 
 const PORT = process.env.PORT || '3002'
 const dev = process.env.NODE_ENV !== 'production'
@@ -18,7 +17,6 @@ function openServer() {
     .use(express.static('public'))
     .use(bodyParser.json())
     .use(cors())
-    .use(forceSSLMiddleware)
     .use('/novo', redirect)
     .use(handler)
     .listen(PORT, () => process.stdout.write(`Server Ready on port:${PORT}\n`))
@@ -26,9 +24,4 @@ function openServer() {
 
 function redirect (req, res) {
   return res.redirect(302, req.url)
-}
-
-function forceSSLMiddleware (req, res, next) {
-  if (process.env.FORCE_SSL !== 'true') return next()
-  return forceSSL(req, res, next)
 }
