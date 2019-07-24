24ecec3a48fc7df07666eeeb5a755a2424f9a66b
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-24 19:46:25 -0300

    Finish

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,10 +1,6 @@
 const spawn = require('child_process').spawn
-const fs = require('fs')
 
-console.log('all: ', process.env)
 if (process.env.HOME === '/root') {
-  fs.unlinkSync('.env')
-
   const ls = spawn('npm', ['run', 'build'])
   ls.stdout.on('data', data => {
     process.stdout.write(data.toString('utf8')
