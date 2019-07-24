a3e0997fa4b519abad1e24a4592e83720efefc4c
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-24 20:34:37 -0300

    Try fix cache

--- public/chalacabar-service-worker.js
+++ public/chalacabar-service-worker.js
@@ -1,16 +1,23 @@
-const cacheName = 'chalaca'
+const cacheName = 'chalacabar'
 const filesToCache = ['/css/style.css', '/img/logo/logo-2019-white-transparent.png']
 self.addEventListener('install', function(e) {
   fetch('https://chalacabar.com.br/api/install', { method: 'POST' })
+  fetch('https://chalacabar.com.br/.act/install')
   e.waitUntil(
     caches.open(cacheName).then(cache => {
+      cache.add(new Request('/', { redirect: 'follow' }))
       cache.addAll(filesToCache)
     })
   )
 })
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim())
+  fetch('https://chalacabar.com.br/.act/activate')
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.delete('/')
+      self.clients.claim()
+    }))
 })
 
 self.addEventListener('fetch', event => {
@@ -19,6 +26,7 @@ self.addEventListener('fetch', event => {
   )
 })
 
-self.addEventListener('sync', function(e) {
+self.addEventListener('sync', e => {
+  fetch('https://chalacabar.com.br/.act/sync')
   e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(filesToCache)))
 })
