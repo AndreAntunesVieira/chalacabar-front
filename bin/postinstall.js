f9d9d175f78a46bba6a1b1d4b1a9c8eaca5849ee
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-24 14:40:06 -0300

    .env generator

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,6 +1,14 @@
 const spawn = require('child_process').spawn
+const fs = require('fs')
 
 if (process.env.NODE_ENV === 'production') {
+  let env = fs.readFileSync('.env.sample', 'utf8').split('\n')
+  env = env.filter(row => row).map(row => {
+    const [name] = row.split('=')
+    return `${name}=${process.env[name]}`
+  })
+  fs.writeFileSync('.env', env.join('\n'))
+
   const ls = spawn('npm', ['run', 'build'])
   ls.stdout.on('data', data => {
     process.stdout.write(data.toString('utf8')
