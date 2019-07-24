137f95e30eaa77f703b5e521d40833d52216df29
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-24 01:50:55 -0300

    Add enforce ssl

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const next = require('next')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const forceSSL = require('express-force-ssl')
 
 const PORT = process.env.PORT || '3002'
 const dev = process.env.NODE_ENV !== 'production'
@@ -17,12 +18,17 @@ function openServer() {
     .use(express.static('public'))
     .use(bodyParser.json())
     .use(cors())
+    .use(forceSSLMiddleware)
     .use('/novo', redirect)
     .use(handler)
     .listen(PORT, () => process.stdout.write(`Server Ready on port:${PORT}\n`))
 }
 
-
-function redirect(req, res){
+function redirect (req, res) {
   return res.redirect(302, req.url)
+}
+
+function forceSSLMiddleware (req, res, next) {
+  if (process.env.FORCE_SSL !== 'true') return next()
+  return forceSSL(req, res, next)
 }
