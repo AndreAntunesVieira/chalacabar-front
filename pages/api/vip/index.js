d01ec8d13b55e532d5dbd1679b199e744c3e2019
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-07-26 15:55:35 -0300

    Send created vip

--- pages/api/vip/index.js
+++ pages/api/vip/index.js
@@ -1,11 +1,13 @@
 import VipModel from 'models/VipModel'
 import { sendCreated, sendUnprocessableEntity } from 'helpers/api/SendResponse'
+import { requestJson } from 'helpers/RequestHelpers'
 
 export default (req, res) => {
-  const { partyId, tableNumber } = req.query
+  const query = requestJson(req)
+  const { partyId, tableNumber } = query
   return new VipModel()
     .isFree(partyId, tableNumber)
-    .then(() => new VipModel().create(req.query))
+    .then(() => new VipModel().create(query))
     .then(sendCreated(res))
     .catch(e => {
       if (e === false) return sendUnprocessableEntity(res, 'Desculpe, alguém acabou de reservar esta mesa :(')
