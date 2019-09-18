e17ca8230524fe3be2dc55a2bd4d96af8b7ae66b
Andre Antunes Vieira <andreantunesv@gmail.com>
2019-09-18 15:13:23 -0300

    Fix cache

--- public/chalacabar-service-worker.js
+++ public/chalacabar-service-worker.js
@@ -1,6 +1,6 @@
 const cacheName = 'chalacabar'
 const filesToCache = ['/css/style.css', '/img/logo/logo-2019-white-transparent.webp']
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
   fetch('https://chalacabar.com.br/api/install', { method: 'POST' })
   fetch('https://chalacabar.com.br/.act/install')
   e.waitUntil(
@@ -13,29 +13,10 @@ self.addEventListener('install', function(e) {
 })
 
 self.addEventListener('activate', event => {
-  fetch('https://chalacabar.com.br/.act/activate')
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.delete('/')
-      self.clients.claim()
-    }))
+  event.waitUntil(caches.keys().then(cacheNames => Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)))))
 })
 
 self.addEventListener('fetch', event => {
-  if(event.request.url === '/'){
-    return event.respondWith(
-      caches.open(cacheName)
-        .then(cache => {
-          try{
-            cache.delete('/')
-          }
-          catch(e){
-
-          }
-          return fetch(event.request)
-        })
-    )
-  }
   event.respondWith(
     caches.match(event.request, { ignoreSearch: true }).then(response => response || fetch(event.request))
   )
